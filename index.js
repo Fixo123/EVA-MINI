d848fdf957e5e49d0c2d70da72d8dfde6de83d3e
FIXO DEV <videodd42@gmail.com>
2026-09-01 23:41:34 +0530

    Add slleak command to index.js

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ const commands = {
     alive: require('./commands/alive'),
     jid: require('./commands/jid'),
     getjid: require('./commands/jid'),
-    movie: require('./commands/movie')
+    movie: require('./commands/movie'),
+    slleak: require('./commands/slleak')
 };
 
 
@@ -520,6 +521,7 @@ if (!isMe && !isStatus) {
                    `╭━━━〔 ${toBold("𝗧𝗢𝗢𝗟𝗦")} 〕━━━┈⊷\n` +
                    `┃ ⋄ ${toBold(".𝗮𝗽𝗸 (𝗻𝗮𝗺𝗲)")}\n` +
                    `┃ ⋄ ${toBold(".𝗷𝗶𝗱")}\n` +
+                   `┃ ⋄ ${toBold("..𝘀𝗹𝗹𝗲𝗮𝗸")}\n` +
                    `┃ ⋄ ${toBold(".𝗺𝗼𝘃𝗶𝗲 [𝗻𝗮𝗺𝗲]")}\n` +
                    `┃ ⋄ ${toBold(".𝗳𝗮𝗰𝗲𝗯𝗼𝗼𝗸 (𝘂𝗿𝗹)")}\n` +
                    `┃ ⋄ ${toBold(".𝘁𝗶𝗸𝘁𝗼𝗸 (𝘂𝗿𝗹)")}\n` +
@@ -633,6 +635,7 @@ if (!isMe && !isStatus) {
                                         case 'jid':
                                         case 'getjid':await commands.jid(this.sock, from, msg, args); break;
                                         case 'movie': await commands.movie(this.sock, from, msg, args, isAdmin, botData); break;
+                                        case 'slleak':await commands.slleak(this.sock, from, msg, args, isAdmin, botData); break;
 
 
 
